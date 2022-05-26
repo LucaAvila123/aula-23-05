a671ebc944641db6e0f3373e797be2fbb6dd000e
Luca <91071096+LucaAvila123@users.noreply.github.com>
2022-05-26 18:28:25 -0300

    Update lista.c

--- lista.c
+++ lista.c
@@ -1,103 +1,86 @@
-#include "pokemon.h"
 #include "lista.h"
+#include "celula.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-//estrutura da célula
-struct cel{
-    tPokemon* pokemon;
-    Celula* prox;
-};
-
-//lista com sentinela
 struct lista{
     Celula* prim;
     Celula* ult;
 };
 
-Lista* inicializaListaVazia(){
-    Lista* lista = (Lista*) malloc(sizeof(Lista));
+struct cel{
+    Aluno* aluno;
+    Celula* prox;
+};
+
+Lista* inic_Lista(){
+    Lista* lista = (Lista*) malloc (sizeof(Lista));
     lista->prim = NULL;
-    lista->ult  = NULL;
-    //printf("Inicializou, aaaaaaa\n");
+    lista->ult = NULL;
     return lista;
 }
 
-void insere(Lista* lista, tPokemon* pokemon){
-    Celula* nova = (Celula*) malloc (sizeof(Celula));
-    nova->pokemon = pokemon;
-
-    //inserindo um elemento ao final da lista;
-    if(lista->prim == NULL){
-       lista->prim = nova;
-       lista->ult = lista->prim;
-    }
-
-    else{
-        lista->ult->prox = lista->ult;
-        lista->ult = lista->ult->prox = nova;
-        nova->prox = NULL;
-    }
+void insere(Lista* lista, Aluno* aluno){
+    Celula* nova = (Celula*) malloc(sizeof(Celula));
+    nova->prox = lista->prim;
+    nova->aluno = aluno;
+    lista->prim = nova;
+    //alguma coisa precisa ser a última célula alguma vez
+    if(lista->ult = NULL) lista->ult = nova;
 }
 
-void retira(Lista* lista, char* nome){
+void retira(Lista* lista, int mat){
+    //ponteiro auxiliar para antes e depois (importante para excluir células)
+
     Celula* p = lista->prim;
     Celula* ant = NULL;
 
-    while(p != NULL && strcmp(nome, retornaNome(p->pokemon))){
+    while(p != NULL && RetornaMatricula(p) != mat){
         ant = p;
         p = p->prox;
     }
 
     if(p == NULL){
-        abandona(p->pokemon);
         free(p);
         return;
     }
     if(lista->prim == lista->ult){
         lista->prim = lista->ult = NULL;
-        abandona(p->pokemon);
         free(p);
         return;
     }
     if(lista->prim == p){
         lista->prim = p->prox;
-        abandona(p->pokemon);
         free(p);
         return;
     }
     if(lista->ult == p){
         lista->ult = ant;
         lista->ult->prox = NULL;
-        abandona(p->pokemon);
         free(p);
         return;
     }
 
     ant->prox = p->prox;
-    abandona(p->pokemon);
     free(p);
-
 }
 
 void imprime(Lista* lista){
     Celula* p;
-    for(p=lista->prim; p != NULL; p=p->prox){
-        imprimePokemon(p->pokemon);
-    }
+    for(p=lista->prim; p != NULL; p=p->prox)
+        imprime_Nome(p->aluno);
+    free(p); //precisa disso?
 }
-//criatividade 10 pra funcao de destruir lista
-void release(Lista* lista){
+
+void libera(Lista* lista){
     Celula* p = lista->prim;
     Celula* temp = NULL;
 
     while(p != NULL){
-        abandona(p->pokemon);
         temp = p->prox;
         free(p);
         p = temp;
     }
 
     free(lista);
-}
+} 
